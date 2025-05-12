4c1f8feb9b9cbb731d9630bef3c38caf81075f44
TROHPSO <raphael.telesphort1@gmail.com>
2025-05-12 22:26:11 +0400

    main frontend pages

---
+++ frontend/pages/components/navbar.php
@@ -0,0 +1,16 @@
+<div class="flex flex-col md:flex-row items-center justify-between p-4 bg-white shadow-md">
+        <h1 id="title" class="text-2xl text-gray-800 mb-4 md:mb-0">Moduls</h1>
+        <div id="searchBar" class="flex items-center space-x-2 w-full md:w-auto mb-4 md:mb-0">
+            <input type="text" id="searchInput" class="flex-grow px-4 py-2 border border-gray-300 rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Rechercher un module">
+            <button id="searchButton" class="px-4 py-2 bg-blue-500 text-white rounded-r-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500">Rechercher</button>
+        </div>
+        <div id="authBoutons" class="flex space-x-2 w-full md:w-auto">
+            
+            <a id="loginButton" href="../identification/form_connection.html" class="flex-grow px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500">
+            <button class="w-full h-full bg-transparent border-none focus:outline-none">Se connecter</button></a>    
+            
+            <a href="../identification/form_inscription.html" class="">
+            <button id="registerButton" class=" ">S'inscrire</button></a>
+
+        </div>
+    </div>
