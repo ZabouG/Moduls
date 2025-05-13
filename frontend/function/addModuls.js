427007ec1f9560385003cdef60b98c95cb929fdd
TROHPSO <raphael.telesphort1@gmail.com>
2025-05-13 14:54:16 +0400

    front-end update side-bar main component

---
+++ frontend/function/addModuls.js
@@ -0,0 +1,26 @@
+/* Récupéré élément du DOM  
+
+Fonction getModulsData()
+Quand bouton "Créer" préssé récupére 
+- Titre 
+- Description
+- Contenu
+- Image
+
+Fonction addModuls()
+Après récupération, affichage des données  :
+
+lance une fonction pour ajouter les données dans la base de données en PHP
+
+    Dans page/home.php
+    - Création dynamique d'un modal avec les informations (titre, description et image)
+    Dans page/profil.php 
+    - afficher les données sous forme tableau (Titre, Date, auteur)
+
+Fonction modifModuls()
+Quand bouton "Modifier" préssé affichage d'un pop up avec les données pour les modifiers.
+Quand boton "Supprimer" préssé suppréssion du moduls de la base données avec un popup de confirmation.
+
+*/
+
+
