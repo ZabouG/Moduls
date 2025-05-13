be58e24c162f9154885a3bdcd28d423d15abbafe
TROHPSO <raphael.telesphort1@gmail.com>
2025-05-13 14:55:35 +0400

    frontend update sidebar component

---
+++ frontend/pages/components/addModuls.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+<!-- <div class="flex items-center justify-center overflow-x-auto overflow-y-hidden border-b border-gray-200 whitespace-nowrap dark:border-gray-700">
+    <button class="inline-flex items-center h-10 px-4 -mb-px text-sm text-center text-blue-600 bg-transparent border-b-2 border-blue-500 sm:text-base dark:border-blue-400 dark:text-blue-300 whitespace-nowrap focus:outline-none">
+        Moduls
+    </button>
+
+    <button class="inline-flex items-center h-10 px-4 -mb-px text-sm text-center text-gray-700 bg-transparent border-b-2 border-transparent sm:text-base dark:text-white whitespace-nowrap cursor-base focus:outline-none hover:border-gray-400">
+        Contenu
+    </button>
+
+    <button class="inline-flex items-center h-10 px-4 -mb-px text-sm text-center text-gray-700 bg-transparent border-b-2 border-transparent sm:text-base dark:text-white whitespace-nowrap cursor-base focus:outline-none hover:border-gray-400">
+        Profil
+    </button>
+</div> -->
+</body>
+</html>
